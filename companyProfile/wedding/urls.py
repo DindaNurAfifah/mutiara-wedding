ec9d4d97c8b7fa58852b614bae587d950094253c
Anandiaz <anandiazagung@gmail.com>
2024-11-11 17:39:05 +0800

    Gabungan 1

--- companyProfile/wedding/urls.py
+++ companyProfile/wedding/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("contact", views.contact_view, name="contact"),
     path("team", views.team_view, name="team"),
+    path("about", views.about_view, name="about"),
     path("", views.services_view, name="services"),
 ]
--- companyProfile/wedding/views.py
+++ companyProfile/wedding/views.py
@@ -16,6 +16,12 @@ def team_view(request):
     }
     return render(request, 'team/team.html', context)
 
+def about_view(request):
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL
+    }
+    return render(request, 'about_us/about.html', context)
+
 def services_view(request):
     context = {
         'MEDIA_URL': settings.MEDIA_URL,
